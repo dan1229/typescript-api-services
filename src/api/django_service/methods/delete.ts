ddfeebaad82013beaf803c968d7205e2044d1396
Daniel Nazarian <danielnazarian@outlook.com>
2024-02-14 00:21:28 -0500

    Refactor HTTP methods in Django service***
    ***Add retry logic to HTTP DELETE and GET methods***
    
    ***Add retry logic to HTTP PATCH and POST methods

--- src/api/django_service/methods/delete.ts
+++ src/api/django_service/methods/delete.ts
@@ -3,7 +3,6 @@ import { DjangoApiResponseHandler } from '../django_api_response_handler'
 import { type ApiResponse } from '../../../types'
 
 /**
- *
  * DJANGO DELETE
  *
  * Django Delete - API methods for DELETE requests for a Django API
@@ -15,9 +14,9 @@ export default class DjangoDelete<Model> extends DjangoApi {
    * @param {string} url - URL to call
    * @param {Record<string, unknown>} extraHeaders - Extra headers to add to request
    */
-  protected async httpDelete (url: string, extraHeaders?: Record<string, unknown>): Promise<any> {
-    const headers = this.getHeaders(extraHeaders)
-    return await this.client.delete(url, headers)
+  protected async httpDelete(url: string, extraHeaders?: Record<string, unknown>): Promise<any> {
+    const headers = this.getHeaders(extraHeaders);
+    return await this.retryIfNecessary<Model>(() => this.client.delete(url, headers), url);
   }
 
   /**
@@ -29,14 +28,11 @@ export default class DjangoDelete<Model> extends DjangoApi {
    * @param {Record<string, unknown>} extraHeaders - Extra headers to add to request
    * @return {ApiResponse} Api response object
    */
-  public async deleteItem (id: string, extraHeaders?: Record<string, unknown>): Promise<ApiResponse<Model>> {
-    this.loading = true
-    const url = this.urlApi(id)
-    const response = await this.retryIfNecessary<Model>(
-      () => this.httpDelete(url, extraHeaders),
-      url
-    );
-    this.loading = false
-    return response
+  public async deleteItem(id: string, extraHeaders?: Record<string, unknown>): Promise<ApiResponse<Model>> {
+    this.loading = true;
+    const url = this.urlApi(id);
+    const response = await this.httpDelete(url, extraHeaders);
+    this.loading = false;
+    return response;
   }
 }
