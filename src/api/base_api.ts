d27dc789ad2a9e079f5f1ca2c56ba26273e3fb66
Daniel Nazarian <danielnazarian@outlook.com>
2024-06-15 13:16:18 -0400

    lint

--- src/api/base_api.ts
+++ src/api/base_api.ts
@@ -1,6 +1,6 @@
 import axios, { type AxiosInstance, type AxiosRequestConfig, type AxiosResponse } from 'axios'
 import { BaseApiResponseHandler } from './base_api_response_handler'
-import { ApiResponseDuplicate, type ApiResponse } from '../types'
+import { type ApiResponse } from '../types'
 import { DjangoApiResponseHandler } from './django_service/django_api_response_handler'
 import DjangoApi from './django_service/django_api'
 
@@ -27,6 +27,7 @@ export abstract class BaseApi {
   static lastRequestTimestamps: Record<string, number> = {}
 
   // Maintain a dictionary to store the last successful responses
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
   static lastSuccessfulResponses: Record<string, ApiResponse<any>> = {}
 
   /**
@@ -99,38 +100,38 @@ export abstract class BaseApi {
    * Avoids duplicate requests within a certain time window.
    */
   async catchDuplicates<T>(requestFunction: () => Promise<AxiosResponse>, urlToCall: string): Promise<ApiResponse<T>> {
-    const now = Date.now();
+    const now = Date.now()
     // this accounts for both the page the URL is called on and the URL itself
     // that way if a user is changing pages, the following ID is different and
     // the request will go through
-    const pageUrlId = `${urlToCall}`;
-    const lastRequestTime = BaseApi.lastRequestTimestamps[pageUrlId] || 0;
-  
+    const pageUrlId = `${urlToCall}`
+    const lastRequestTime = BaseApi.lastRequestTimestamps[pageUrlId] || 0
+
     // see if the request is a duplicate
-    const timeElapsed = now - lastRequestTime;
+    const timeElapsed = now - lastRequestTime
     const duplicateCall = timeElapsed < this.minimumDelay
-  
+
     // Handle the response
-    let response: ApiResponse<T>;
+    let response: ApiResponse<T>
     if (!duplicateCall) {
       // Get the response handler
       const responseHandler =
         this instanceof DjangoApi
-        ? new DjangoApiResponseHandler<T>(this, requestFunction())
-        : new BaseApiResponseHandler<T>(this, requestFunction())
-    
+          ? new DjangoApiResponseHandler<T>(this, requestFunction())
+          : new BaseApiResponseHandler<T>(this, requestFunction())
+
       // Update the last request timestamp
-      BaseApi.lastRequestTimestamps[pageUrlId] = now;
+      BaseApi.lastRequestTimestamps[pageUrlId] = now
 
       // Handle the request
-      response = await responseHandler.handleResponse();
+      response = await responseHandler.handleResponse()
 
       // Store the response
-      BaseApi.lastSuccessfulResponses[pageUrlId] = response;
+      BaseApi.lastSuccessfulResponses[pageUrlId] = response
     } else {
-      response = BaseApi.lastSuccessfulResponses[pageUrlId];
+      response = BaseApi.lastSuccessfulResponses[pageUrlId]
     }
-    console.log('res', response)
-    return response;
+
+    return response
   }
 }
